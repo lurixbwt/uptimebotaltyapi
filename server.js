943febfcdde88e14f16336cfc9bcf4804d3511f6
Glitch (garnet-zircon-gasoline) <none>
2020-07-20 14:16:56 +0000

    Import of MertBhey/Uptime

--- server.js
+++ server.js
@@ -1,52 +1,12 @@
-const Discord = require("discord.js");
-const client = new Discord.Client();
-const express = require("express");
-const app = express();
+require("express")().listen(1343);
+
 const db = require("quick.db");
-const fs = require("fs");
+const discord = require("discord.js");
+const client = new discord.Client({ disableEveryone: true });
+client.login("");
+const fetch = require("node-fetch");
+const fs = require('fs')
 
-//Uptime için__________________________________________________________________
-app.get("/", (req, res) => {
-  res.send("HELO");
-});
-app.listen(process.env.PORT);
-
-//KOMUT Algılayıcı______________________________________________________________
-client.commands = new Discord.Collection();
-client.aliases = new Discord.Collection();
-
-fs.readdir("./komutlar/", (err, files) => {
-  if (err) return console.error(err);
-  files.forEach((file) => {
-    if (!file.endsWith(".js")) return;
-    let cmd = require(`./komutlar/${file}`);
-    let cmdFileName = file.split(".")[0];
-    client.commands.set(cmd.help.name, cmd);
-    console.log(`Komut Yükleme Çalışıyor: ${cmdFileName}`);
-    if (cmd.help.aliases) {
-      cmd.help.aliases.forEach(alias => {
-        client.aliases.set(alias, cmd.help.name);
-      });
-    };
-  });
-});
-
-//EVENTS Yükleyici_______________________________________________________________
-fs.readdir("./events/", (err, files) => {
-  if (err) return console.error(err);
-  files.forEach((file) => {
-    const event = require(`./events/${file}`);
-    let eventName = file.split(".")[0];
-    console.log(`Etkinlik Yükleme Çalışıyor: ${eventName}`);
-    client.on(eventName, event.bind(null, client));
-  });
-});
-
-client.on("ready", () => {
-  console.log(`${client.user.tag}! Aktif!`);
-});
-
-client.login(process.env.TOKEN);
 setInterval(() => {
   var links = db.get("linkler");
   if(!links) return;
@@ -68,7 +28,7 @@ db.set("linkler", [])
 client.on("message", message => {
   if(message.author.bot) return;
   var spl = message.content.split(" ");
-  if(spl[0] == "g!ekle") {
+  if(spl[0] == "p!ekle") {
   var link = spl[1]
   fetch(link).then(() => {
     if(db.get("linkler").map(z => z.url).includes(link)) return message.channel.send("zaten vad")

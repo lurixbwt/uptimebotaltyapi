d2748ef9d7c6d6802b8d472698c533a43f39c715
Glitch (garnet-zircon-gasoline) <none>
2020-09-11 10:32:49 +0000

    ⚡️💂 Checkpoint ./server.js:15724350/6

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const http = require('http');
     });
     app.listen(process.env.PORT);
     setInterval(() => {
-    http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+    http.cache.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
     }, 280000);
 
 //Oynuyor

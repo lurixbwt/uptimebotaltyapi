a666bcb25705c707cc08354ce4b97efc60d08ab0
Glitch (garnet-zircon-gasoline) <none>
2020-11-20 15:47:18 +0000

    🍤👳 Checkpoint ./salvouptime.js:16786118/64

--- salvouptime.js
+++ salvouptime.js
@@ -1,7 +1,7 @@
 const db = require("quick.db");
 const discord = require("discord.js");
 const client = new discord.Client({ disableEveryone: true });
-client.login("Nzc5MzYxMjYyMzM3MjYxNjI4.X7favA.lDGf19WVyKCEAngUrYTgoyPR75k");
+client.login("TOKEN");
 const fetch = require("node-fetch");
 const fs = require("fs");
 require("express")().listen(1343);
